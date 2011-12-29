8f85558129cb66fe80fadc8bd0ea9b844289fb97
Braiden Kindt <braiden@braiden.org>
2011-12-29 18:28:30 -0500

    clean-up, plus most config functions

--- src/AntFunction.py
+++ src/AntFunction.py
@@ -1,6 +1,8 @@
 # ANT message spec defined in "ANT Messages Protocol and Usage" Rev 4.5
 
-from struct import pack
+from struct import pack, calcsize
+
+ANT_SYNC_TX = 0xA4
 
 class AntFunction(object):
     """
@@ -20,37 +22,32 @@ class AntFunction(object):
         
     def checksum(self, msg):
         """
-        Generate a check sum of the given input.
-        Per spec, xorg of all data including sync
+        Generate a checksum of the given input.
+        Per spec, xor of all data including sync
         """
-        return reduce(lambda x, y: x ^ y, msg) & 0xFF
+        return reduce(lambda x, y: x ^ y, map(lambda x: ord(x), msg))
 
     def pack(self, *args):
         """
         Return a byte array which represents the data
         needing to be written to device where the give args.
         """
-        data = bytearray(pack(self.arg_pack, *args))
-        sync = 0xA4
-        length = len(data)
-        header = bytearray(pack("BBB", sync, length, self.msg_id))
-        header.extend(data)
-        checksum = self.checksum(header)
-        header.append(checksum)
-        return header
+        length = calcsize(self.arg_pack)
+        data = pack("3B" + self.arg_pack, ANT_SYNC_TX, length, self.msg_id, *args)
+        return data + pack("B", self.checksum(data))
 
-    def execute(self, sink, *args):
+    def execute(self, device, *args):
         """
         Execute this command on the given target (sink)
         """
         data = self.pack(*args)
-        if sink:
+        if device:
             for byte in data:
-                sink.write(byte)
+                device.write(byte)
         return data
 
-    def __call__(self, sink, *args):
-        return self.execute(sink, *args)
+    def __call__(self, device, *args):
+        return self.execute(device, *args)
 
 
 ANT_UnassignChannel = AntFunction(0x41, "B")
@@ -61,9 +58,18 @@ ANT_SetChannelPeriod = AntFunction(0x43, "B<H")
 ANT_SetChannelSearchTimeout = AntFunction(0x44, "BB")
 ANT_SetChannelRfFreq = AntFunction(0x45, "BB")
 ANT_SetNetworkKey = AntFunction(0x46, "B8s")
-ANT_AddChannelId = AntFunction(0x59, "B<HBBB")
+ANT_SetTransmitPower = AntFunction(0x47, "xB")
+ANT_AddChannelId = AntFunction(0x59, "B<HBB?")
 ANT_ConfigList = AntFunction(0x5A, "BBB")
 ANT_SetChannelTxPower = AntFunction(0x60, "BB")
-ANT_SetLowPriorityChannelSearchTimeout(0x63, "BB")
+ANT_SetLowPriorityChannelSearchTimeout = AntFunction(0x63, "BB")
+ANT_SetSerialNumChannelId = AntFunction(0x65, "BBB")
+ANT_RxExtMesgsEnable = AntFunction(0x66, "x?")
+ANT_EnableLed = AntFunction(0x68, "x?")
+ANT_CrystalEnable = AntFunction(0x6D, "x")
+ANT_LibConfig = AntFunction(0x6E, "xB")
+ANT_ConfigFrequencyAgility = AntFunction(0x70, "BBBB")
+ANT_SetProximitySearch = AntFunction(0x71, "BB")
+ANT_SetChannelSearchPriority = AntFunction(0x75, "BB")
 
 # vim: et ts=4 sts=4
