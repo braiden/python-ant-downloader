6d063bbfac5f52da73ab03d7cabcf0eab67646ee
Braiden Kindt <braiden@braiden.org>
2011-12-29 21:24:02 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name = "anttools",
+    version = "0.0.1",
+	description = "garmin-ant-tools",
+	author = "Braiden Kindt",
+	url = "http://github.com/braiden/garmin-ant-tools",
+	license = "MIT",
+	packages = ["anttools"],
+	scripts = []
+)
