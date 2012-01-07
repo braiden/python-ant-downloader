f18cc18030073ebd4b9900e25acc4ceebd9aeea8
Braiden Kindt <braiden@braiden.org>
2012-01-07 10:05:08 -0500

    cleanup

--- gant/ant.py
+++ gant/ant.py
@@ -1,3 +1,5 @@
+from mock import Mock
+
 class Device(object):
     """
     An Ant_Device provides high-level access to the ANT
@@ -6,18 +8,21 @@ class Device(object):
     availible for radio communication.
     """
 
+    max_networks = 0
+    max_channels = 0
+
     _all_channels = []
     _all_networks = []
     _free_channels = []
     _free_networks = []
 
-    def __init__(self, dispatcher):
+    def __init__(self, dialect):
         """
         Create a new Ant_Device which dispatches messages
         through the given dispatcher.
         """
-        self._dispatcher = dispatcher
-        reset_device()
+        self._dialect = dialect 
+        self.reset_device()
 
     def claim_network(self):
         """
@@ -27,7 +32,7 @@ class Device(object):
         remain claimed as long it is bound to any active
         channels.
         """
-        return _free_channels.pop() if _free_channels else none
+        return self._free_networks.pop() if self._free_networks else None
 
     def claim_channel(self):
         """
@@ -36,29 +41,80 @@ class Device(object):
         complete. Channel provides the primary interface
         to find devices and rx/tx data.
         """
-        return _free_networks.pop() if _free_networks else none
+        return self._free_channels.pop() if self._free_channels else None
 
     def release_network(self, network):
         """
         Relase the given network. Raises error
         if any allocated channel is assigned this network
         """
-        assert not [c for c in _all_channels if c._network == network]
-        _free_networks.append(network(self, network._network_id, self._dispatcher))
+        assert not [c for c in self._all_channels if c.network == network]
+        network.network_key = "\x00" * 8
+        self._all_networks.remove(network)
+        self._add_network(network._network_id)
 
     def release_channel(self, channel):
         """
         Release the channel for use by another client.
         """
         channel.close()
-        _free_channels.append(channel(self, channel._channel_id, self._dispatcher))
+        self._all_channels.remove(channel)
+        self._add_channel(channel._channel_id)
 
     def reset_device(self):
         """
         Reset the device, invalidating any currently allocated
         channels or networks.
         """
-        self._dispatcher.send(ANT_RESET_SYSTEM)
+        self._dialect.reset_system()
+
+    def channel(self):
+        class ConnectionContextManager(object):
+            def __init__(self, device):
+                self._device = device
+            def __enter__(self):
+                self._channel = self._device.claim_channel()
+                return self._channel
+            def __exit__(self, exc_type, exc_value, traceback):
+                self._device.release_channel(self._channel)
+        return ConnectionContextManager(self)
+
+    def network(self):
+        class NetworkContextManager(object):
+            def __init__(self, device):
+                self._device = device
+            def __enter__(self):
+                self._network = self._device.claim_network()
+                return self._network
+            def __exit__(self, exc_type, exc_value, traceback):
+                self._device.release_network(self._network)
+        return NetworkContextManager(self)
+
+    def is_valid_network(self, network):
+        return network in self._all_networks and network not in self._free_channels
+
+    def is_valid_channel(self, channel):
+        return channel in self._all_channels and channel not in self._free_channels
+
+    def _init_pools(self, max_networks, max_channels):
+        self.max_networks = max_networks
+        self.max_channels = max_channels
+        self._all_networks = []
+        self._all_channels = []
+        self._free_networks = []
+        self._free_channels = []
+        for n in range(0, self.max_networks): self._add_network(n)
+        for n in range(0, self.max_channels): self._add_channel(n)
+         
+    def _add_channel(self, channel_id):
+        channel = Channel(channel_id, self, self._dialect)
+        self._free_channels.append(channel)
+        self._all_channels.append(channel)
+    
+    def _add_network(self, network_id):
+        network = Network(network_id, self, self._dialect)
+        self._free_networks.append(network)
+        self._all_networks.append(network)
 
 
 class Channel(object):
@@ -66,7 +122,7 @@ class Channel(object):
     An ANT communications channel.
     """
 
-    network = none
+    network = None
     channel_type = 0
     network = 0
     device_number = 0
@@ -76,21 +132,15 @@ class Channel(object):
     search_timeout = 255
     rf_freq_mhz = 2466
 
-    def __init__(self, channel_id, device, dispatcher):
+    def __init__(self, channel_id, device, dialect):
         self._channel_id = channel_id
         self._device = device
-        self._dispatcher = dispatcher 
+        self._dialect = dialect
 
     def is_valid(self):
-        """
-        True if the channel is property aquired.
-        """
-        return (self in self._device._all_channels
-            and self not in self._device._free_channels
-            and self._network in self._device._all_networks
-            and self._network not in self._device._free_networks)
+        return self._device.is_valid_channel(self)
 
-    def open():
+    def open(self):
         """
         Apply all setting to this channel and open
         for communication. If channel is alreayd openned
@@ -98,44 +148,40 @@ class Channel(object):
         """
         assert self.is_valid()
         self.close()
-        self._dispatcher.send(ANT_ASSIGN_CHANNEL, self._channel_id, self.channel_type, self.network)
-        self._dispatcher.send(ANT_SET_CHANNEL_ID, self._channel_id, self.device_number, self.device_type_id, self.trans_type)
-        self._dispatcher.send(ANT_SET_CHANNEL_PERIOD, self._channel_id, 32768 / self.period_hz)
-        self._dispatcher.send(ANT_SET_CHANNEL_SEARCH_TIMEOUT, self._channel_id, self.search_timeout)
-        self._dispatcher.send(ANT_SET_CHANNEL_RF_FREQ, self._channel_id, self.rf_freq_mhz - 2400)
-        self._dispatcher.send(ANT_OPEN_CHANNEL, self._channel_id)
+        self._dialect.assign_channel(self._channel_id, self.channel_type, self.network)
+        self._dialect.set_channel_id(self._channel_id, self.device_number, self.device_type, self.trans_type)
+        self._dialect.set_channel_period(self._channel_id, self.period_hz)
+        self._dialect.set_channel_search_timeout(self._channel_id, self.search_timeout)
+        self._dialect.set_channel_rf_freq(self._channel_id, self.rf_freq_mhz)
+        self._dialect.open_channel(self._channel_id)
 
-    def close():
+    def close(self):
         """
-        Close the channel, no further async events will happen.
+        close the channel, no further async events will happen.
         """
-        self._dispatcher.send(ANT_CLOSE_CHANNEL, self._channel_id)
+        self._dialect.close_channel(self._channel_id)
 
 
 class Network(object):
 
-    def __init__(self, network_id, device, dispacther):
+    def __init__(self, network_id, device, dialect):
         self._network_id = network_id
         self._device = device
-        self._dispatcher = dispatcher
+        self._dialect = dialect
         self._network_key = "\x00" * 8
 
     def is_valid(self):
-        """
-        True if this instance is properly claimed.
-        """
-        return (self in self._device._all_networks
-            and self not in self._device._free_networks)
+        return self._device.is_valid_network(self)
 
     @property
     def network_key(self):
-        def fget(self):
-            return self._network_key
-        def fset(self, network_key):
-            assert self.is_valid()
-            self._network_key = network_key
-            self._dispather.send(ANT_SET_NETWORK_KEY, self._network_id, self._network_key)
-        return locals()
+        return self._network_key
+
+    @network_key.setter
+    def network_key(self, network_key):
+        assert self.is_valid()
+        self._network_key = network_key
+        self._dialect.set_network_key(self._network_id, self._network_key)
 
 
 class SerialDialect(object):
@@ -143,29 +189,31 @@ class SerialDialect(object):
     def __init__(self):
         pass
 
-    def pack(self, msg_id, *args)
+    def pack(self, msg_id, *args):
         pass
 
-    def unpack(self, msg)
-        pass
-
-    def reply_matcher(self, msg_id)
+    def unpack(self, msg):
         pass
 
 
 class Dispatcher(object):
 
-    def __init__(self, dialect, hardware):
-        self._dialect = dialect
+    def __init__(self, hardware):
         self._hardware = hardware
-        self._output = Queue()
 
-    def send(self, msg_id, *args):
-        msg = self._dialect.pack(msg_id, *args)
+    def send(self, msg):
         self._hardware.write(msg)
         
-    def future_on(self, matcher):
+    def notify(self, matcher, callback):
         pass
 
+
+dialect = Mock()
+dev = Device(dialect)
+dev._init_pools(max_networks=3, max_channels=8)
+with dev.network() as network:
+    with dev.channel() as channel:
+       	channel.network = network
+        channel.open()
 
 # vim: et ts=4 sts=4 nowrap
