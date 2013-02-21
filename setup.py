d500812d435a287e1d18ef0746b15ff7636fed92
Braiden Kindt <braiden@braiden.org>
2013-02-21 17:16:57 -0500

    cleaup setup.py
    - update dependencies
    - remove ez_setup (fails from pip via pypi)
    - update dev rules to newer format
    - update change log

--- setup.py
+++ setup.py
@@ -1,34 +1,32 @@
-#!/usr/bin/python
-import ez_setup
-ez_setup.use_setuptools()
+#!/usr/bin/env python
 
-from setuptools import setup
+from distutils.core import setup
 
 setup(
-	name = "python_ant_downloader",
-	version = "12.03.25",
-	author = "Braiden Kindt",
-	author_email = "braiden@braiden.org",
-	description = "Tools for download from wireless Garmin (ANT) GPS devices.",
-	url = "https://github.com/braiden/python-ant-downloader",
-	license = "BSD",
-	keywords = "ANT Garmin GPS 405 405CX 410",
-
-	packages = ["antd"],
-	package_data = {"antd": ["*.cfg"]},
-	entry_points = {
-		'console_scripts': ['ant-downloader = antd.main:downloader']
-	},
-
-	install_requires = [
-		"poster",
-		"argparse",
-		"lxml",
-		"pyusb>=1.0.0a2",
-	],
-	classifiers = [
-		"Development Status :: 4 - Beta",
-		"Environment :: Console",
-		"License :: OSI Approved :: BSD License",
-	],
+    name = "python_ant_downloader",
+    version = "git",
+    author = "Braiden Kindt",
+    author_email = "braiden@braiden.org",
+    description = "Tools for download from wireless Garmin (ANT) GPS devices.",
+    url = "https://github.com/braiden/python-ant-downloader",
+    license = "BSD",
+    keywords = "ANT Garmin GPS 405 405CX 410",
+    packages = ["antd"],
+    package_data = {"antd": ["*.cfg"]},
+    entry_points = {
+        'console_scripts': ['ant-downloader = antd.main:downloader']
+    },
+    install_requires = [
+        "distribute",
+        "poster",
+        "argparse",
+        "lxml",
+        "pyserial",
+        "pyusb>=1.0.0a2",
+    ],
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "License :: OSI Approved :: BSD License",
+    ]
 )
