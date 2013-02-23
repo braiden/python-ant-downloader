8e45d9232125fbaa58272247e04b786ff8dc8e6a
Braiden Kindt <braiden@braiden.org>
2013-02-22 21:43:31 -0500

    minor cleanups

--- antd/notif.py
+++ antd/notif.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2012, Ivan Kelly <ivan@ivankelly.net>
+#               2013, Braiden Kindt <braiden@braiden.org>
 # All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
@@ -39,30 +40,27 @@ class NotifPlugin(plugin.Plugin):
 
     def __init__(self):
         self._enabled = True
-        if not pynotify.init("icon-summary-body"):
+        if not pynotify.init("python-ant-downloader"):
             _log.error("Couldn't enabled pynotify, disabling")
             self._enabled = False
 
     def data_available(self, device_sn, format, files):
-        processed = []
-
         if not self._enabled:
-            return
-
-        if not format.startswith("notif_"): return files
+            return files
+        
         try:
-            filenames = map (lambda(x): os.path.basename(x), files)
+            filenames = map(os.path.basename, files)
             if format == "notif_connect":
                 n = pynotify.Notification(
                     "Ant+ Downloader",
                     "Uploaded files [%s] to Garmin Connect" % ", ".join(filenames),
                     "notification-message-im")
                 n.show()
-            else:
-                n = pynotify.Notification(
-                    "Ant+ Downloader",
-                    "Files [%s] processed" % ", ".join(filenames), 
-                    "notification-message-im")
-                n.show()
+#           elif format == "tcx":
+#               n = pynotify.Notification(
+#                   "Ant+ Downloader",
+#                   "Files [%s] processed" % ", ".join(filenames), 
+#                   "notification-message-im")
+#               n.show()
         finally:
             return files
